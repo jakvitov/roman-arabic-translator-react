b9b000cd8752c2ff8fc7014a2694bfe2eb3d3a53
Jakub Vítovec <jakvitov@gmail.com>
2023-04-17 19:29:00 +0200

    Input field component setup

--- src/componenets/InputField.js
+++ src/componenets/InputField.js
@@ -1,4 +1,10 @@
 function InputField(props) {
 
+    function handleChange(e){
+        props.setInputNumber(e.target.value);
+    }
+
+    return <input type="text" id="inputField" aria-label="Enter number" onChange={handleChange}/>;
+
 }
 export default InputField;
