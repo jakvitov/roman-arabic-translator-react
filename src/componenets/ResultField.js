58c3f1dc3794596a21e5e1721cbe6ed4e493ffae
Jakub Vítovec <jakvitov@gmail.com>
2023-04-17 19:27:59 +0200

    Result field component setup

--- src/componenets/ResultField.js
+++ src/componenets/ResultField.js
@@ -1,5 +1,15 @@
 function ResultField(props) {
 
+    let message;
+
+    if (!props.inputNumber){
+        message = "Enter number"
+    }
+    else {
+        message = props.inputNumber;
+    }
+
+    return <label for="inputField" id="resultField">{message}</label>
 }
 
 export default ResultField;
